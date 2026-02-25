45023e72a7db1377037273bad3e4c53aa815cb95
Nayla Zakiyah Andani <h1101241002@student.untan.ac.id>
2026-02-25 04:42:42 +0000

    update index

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(nama) {
+    return `Selamat Datang ${nama} di PWL` + nama;
+}
+console.log(hello('Jisung'));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function hello(nama: string, umur: number): string{
+    return 'Selamat datang di PWL, umur ' +nama+ ' adalah = ' +umur;
+}
+const mahasiswa = "Wonwoo";
+const umur:number = 19;
+console.log(hello(mahasiswa,umur));
